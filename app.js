9554421364c4e40673cf4868c77abe4cc5f487bc
Nickelz <xinickelz@gmail.com>
2019-08-19 06:55:30 +0300

    MylonaBot now sends a message if its channel was empty, and updates the message otherwise

--- app.js
+++ app.js
@@ -2,61 +2,103 @@ const { Client, RichEmbed} = require("discord.js")
 const moment = require("moment")
 const request = require("request-promise")
 const _ = require("lodash")
+const socket = require("socket.io-client")
 
 const Secret = require("./auth.json")
 
 const client = new Client()
+var embed
 
 var embedInfo = {
 	onlineStatus: false,
 	playerCount: -1,
 	playerMax: 60,
-	serverIP: "vanilla.mylona.fun",
+	serverIP: "us.mineplex.com",
 	serverFavicon: ""
 }
 
 var options = {
-	"url": "https://eu.mc-api.net/v3/server/ping/us.mineplex.com",
+	"url": `https://eu.mc-api.net/v3/server/ping/${embedInfo.serverIP}`,
 	"json": true
 }
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
-	
 
-	request(options).then((body) => {
-		var data = _.pick(body, ["online", "favicon", "players.online", "players.max"])
-		embedInfo.onlineStatus = data.online
-		embedInfo.serverFavicon = data.favicon
-
-		embedInfo.playerMax = embedInfo.onlineStatus ? data.players.max : 0
-		embedInfo.playerCount = embedInfo.onlineStatus ? data.players.online : 0
-
-		const embed = new RichEmbed()
-		.setTitle("Vanilla Server Status")
-		.setColor('GREEN')
-		.addField("Status", embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline")
-		.addField("Player Count", `${embedInfo.playerCount}/${embedInfo.playerMax}`)
-		.addField("Server IP", embedInfo.serverIP)
-		.setFooter(`We're in Beta but would love to have you! — ${moment().format('DD/MM/YY LT')}`, embedInfo.serverFavicon)
-
-		// client.channels.get("612823941941035046").send(embed)
-
-		client.channels.get("612823941941035046").fetchMessage("612836104806334505").then((message) => {
-			updateEmbed(message)
+	client
+		.channels
+		.get("612823941941035046")
+		.fetchMessages({ limit: 10 })
+		.then((messages) => {
+			if (messages.size > 0) {
+				// - EDIT MESSAGE
+				updateEmbed(messages.first())
+				
+			} else {
+				// - CREATE NEW MESSAGE
+				embed = new RichEmbed()
+					.setTitle("Vanilla Server Status")
+					.setColor('GREEN')
+					.addField("Status", embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline")
+					.addField("Player Count", `${embedInfo.playerCount}/${embedInfo.playerMax}`)
+					.addField("Server IP", embedInfo.serverIP)
+					.setFooter(`We're in Beta but would love to have you! — ${moment().format('DD/MM/YY LT')}`, embedInfo.serverFavicon)
+				
+				client
+					.channels
+					.get("612823941941035046")
+					.send(embed)
+					.then((message) => {
+						updateEmbed(message)
+					})
+			}
 		})
-	}).catch((error) => {
-		console.error(error)
-	})
+		.catch((error) => {
+			console.error(error)
+		})
 })
 
+function getDataFromAPI(callback) {
+	request(options).then((body) => {
+		var data = _.pick(body, ["online", "favicon", "players.online", "players.max"]);
+
+		embedInfo.onlineStatus = data.online;
+		embedInfo.serverFavicon = data.favicon;
+		embedInfo.playerMax = embedInfo.onlineStatus ? data.players.max : 0;
+		embedInfo.playerCount = embedInfo.onlineStatus ? data.players.online : 0;
+
+		callback()
+	}).catch((error) => {
+		console.error(error);
+	});
+}
+
 function updateEmbed(message) {
-	var x = 1
 	setInterval(() => {
-		message.embeds[0].fields[2].value = x
-		message.edit(new RichEmbed(message.embeds[0]))
-		x += 1
-	}, 1 * 1000);
+		getDataFromAPI(() => {
+			message
+				.embeds[0]
+				.fields[0]
+				.value = embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline"
+	
+			message
+				.embeds[0]
+				.fields[1]
+				.value = `${embedInfo.playerCount}/${embedInfo.playerMax}`
+	
+			message
+				.embeds[0]
+				.footer
+				.text = `We're in Beta but would love to have you! — ${moment().format('DD/MM/YY LT')}`
+				
+			message
+				.embeds[0]
+				.footer
+				.iconURL = embedInfo.serverFavicon
+	
+			message.edit(new RichEmbed(message.embeds[0]))
+		})
+	}, 3 * 1000)
 }
 
 client.login(Secret.token)
