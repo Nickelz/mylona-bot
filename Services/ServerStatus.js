6ee189d3dc3ba3d1408586b6bcf28be91cd846e0
Nickelz <xinickelz@gmail.com>
2019-08-21 09:33:50 +0300

    Refactored the code and fixed a few issues

---
+++ Services/ServerStatus.js
@@ -0,0 +1,95 @@
+const { RichEmbed } = require("discord.js")
+const moment = require("moment")
+const request = require("request-promise")
+const _ = require("lodash")
+
+const { client } = require("./../app.js")
+const embedInfo = require("./../Objects/EmbedInfo.json")
+
+var embed = null
+
+
+var options = {
+	"url": `https://mcapi.us/server/status?ip=${embedInfo.serverIP}`,
+	"json": true
+}
+
+client.on('ready', () => {
+	console.info(`Server IP: ${options.url}`)
+
+	getDataFromAPI(_.noop)
+})
+
+client.on('message', (message) => {
+	if (message.content === "!mj") {
+		if (message.member.roles.some(role => role.name === "Mylona Staff")) {
+
+			if (embed == null) {
+				embed = new RichEmbed()
+					.setTitle("Vanilla Server Status")
+					.setColor('GREEN')
+					.addField("Status", embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline")
+					.addField("Player Count", `${embedInfo.playerCount}/${embedInfo.playerMax}`)
+					.addField("Server IP", embedInfo.serverIP)
+					.setFooter(`${embedInfo.motd} — ${moment().format('DD/MM/YY LT')}`, embedInfo.favicon)
+			}
+
+			message.channel.send(embed).then((message) => {
+				updateEmbed(message)
+			}).catch((error) => {
+				console.log(`Nope 3 -> ${error}`)
+			})
+
+			message.delete()
+		}
+	}
+
+})
+
+var getDataFromAPI = (callback) => {
+	request(options).then((body) => {
+		var data = _.pick(body, ["status", "online", "players.now", "players.max", "motd"]);
+
+		embedInfo.onlineStatus = data.online;
+		embedInfo.playerMax =  data.players.max
+		embedInfo.playerCount = data.players.now
+		embedInfo.motd = data.motd
+
+		callback()
+	}).catch((error) => {
+		console.log("Nope 4")
+	})
+}
+
+var updateEmbed = (message) => {
+	setInterval(() => {
+		getDataFromAPI(() => {
+			message
+				.embeds[0]
+				.fields[0]
+				.value = embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline"
+	
+			message
+				.embeds[0]
+				.fields[1]
+				.value = `${embedInfo.playerCount}/${embedInfo.playerMax}`
+
+			message
+				.embeds[0]
+				.fields[2]
+				.value = `${embedInfo.serverIP}`
+	
+			message
+				.embeds[0]
+				.footer
+				.text = `${embedInfo.motd} — ${moment().format('DD/MM/YY LT')}`
+				
+			message
+				.embeds[0]
+				.footer
+				.iconURL = embedInfo.favicon
+	
+			message.edit(new RichEmbed(message.embeds[0]))
+		})
+	}, 4 * 60 * 1000)
+}
--- app.js
+++ app.js
@@ -1,104 +1,16 @@
-const { Client, RichEmbed} = require("discord.js")
-const moment = require("moment")
-const request = require("request-promise")
-const _ = require("lodash")
-const socket = require("socket.io-client")
-
-const Secret = require("./auth.json")
+const { Client } = require("discord.js")
+const Secret = require("./Objects/Auth.json")
 
 const client = new Client()
-var embed
-
-var embedInfo = {
-	onlineStatus: false,
-	playerCount: -1,
-	playerMax: 60,
-	serverIP: "us.mineplex.com",
-	serverFavicon: ""
-}
-
-var options = {
-	"url": `https://eu.mc-api.net/v3/server/ping/${embedInfo.serverIP}`,
-	"json": true
-}
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
-
-	client
-		.channels
-		.get("612823941941035046")
-		.fetchMessages({ limit: 10 })
-		.then((messages) => {
-			if (messages.size > 0) {
-				// - EDIT MESSAGE
-				updateEmbed(messages.first())
-				
-			} else {
-				// - CREATE NEW MESSAGE
-				embed = new RichEmbed()
-					.setTitle("Vanilla Server Status")
-					.setColor('GREEN')
-					.addField("Status", embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline")
-					.addField("Player Count", `${embedInfo.playerCount}/${embedInfo.playerMax}`)
-					.addField("Server IP", embedInfo.serverIP)
-					.setFooter(`We're in Beta but would love to have you! — ${moment().format('DD/MM/YY LT')}`, embedInfo.serverFavicon)
-				
-				client
-					.channels
-					.get("612823941941035046")
-					.send(embed)
-					.then((message) => {
-						updateEmbed(message)
-					})
-			}
-		})
-		.catch((error) => {
-			console.error(error)
-		})
 })
 
-function getDataFromAPI(callback) {
-	request(options).then((body) => {
-		var data = _.pick(body, ["online", "favicon", "players.online", "players.max"]);
-
-		embedInfo.onlineStatus = data.online;
-		embedInfo.serverFavicon = data.favicon;
-		embedInfo.playerMax = embedInfo.onlineStatus ? data.players.max : 0;
-		embedInfo.playerCount = embedInfo.onlineStatus ? data.players.online : 0;
-
-		callback()
-	}).catch((error) => {
-		console.error(error);
-	});
-}
-
-function updateEmbed(message) {
-	setInterval(() => {
-		getDataFromAPI(() => {
-			message
-				.embeds[0]
-				.fields[0]
-				.value = embedInfo.onlineStatus ? ":white_check_mark: Online" : ":red_circle: Offline"
-	
-			message
-				.embeds[0]
-				.fields[1]
-				.value = `${embedInfo.playerCount}/${embedInfo.playerMax}`
-	
-			message
-				.embeds[0]
-				.footer
-				.text = `We're in Beta but would love to have you! — ${moment().format('DD/MM/YY LT')}`
-				
-			message
-				.embeds[0]
-				.footer
-				.iconURL = embedInfo.serverFavicon
-	
-			message.edit(new RichEmbed(message.embeds[0]))
-		})
-	}, 3 * 1000)
-}
-
 client.login(Secret.token)
+
+module.exports = {
+	client,
+}
+
+require("./Services/ServerStatus")
